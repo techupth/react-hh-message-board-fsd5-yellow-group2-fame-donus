ca07aa7e6a0dc6bd306309cd69cbe783f917c710
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2023-12-21 18:51:14 +0700

    feat:message board

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,52 @@
+import { useState } from "react"; 
+
 function MessageBoard() {
+  const [todo,setTodo] = useState([]);
+  const [todoText,setTodoText] = useState("");
+
+  const handleTodoText = (event) => {
+    setTodoText(event.target.value);
+  };
+
+  const addTodo = () => {
+    const newTodo = [...todo];
+    newTodo.push(todoText);
+    setTodo(newTodo);
+  }
+
+  const deleteTodo = (index) => {
+    const newTodo = [...todo];
+    newTodo.splice(index,1);
+    setTodo(newTodo);
+  }
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            onChange={handleTodoText}
+            value={todoText}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addTodo}>Submit</button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+      {
+        todo.map((item,index) => {
+          return <div className="message">
+            <h1>{item}</h1>
+            <button className="delete-button" onClick={() => {
+              deleteTodo(index);
+            }}>x</button>
+            </div>
+          })
+        }
       </div>
     </div>
   );
