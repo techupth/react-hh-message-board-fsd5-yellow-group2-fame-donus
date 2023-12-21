5ebfe3051d768fede5a965ad979e811cf1823a55
peerawet <peerawet1996@gmail.com>
2023-12-21 19:42:37 +0700

    finish

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,21 @@
+import { useState, useEffect } from "react";
 function MessageBoard() {
+  const [message, setMessage] = useState();
+  const [allMessgeArray, setAllMessgeArray] = useState([]);
+  useEffect(() => {
+    console.log(allMessgeArray);
+  }, [allMessgeArray]);
+  function handleSubmit() {
+    const currentMessageAllay = [...allMessgeArray, message];
+    setAllMessgeArray(currentMessageAllay);
+    console.log(allMessgeArray);
+  }
+  function handleClose(index) {
+    const currentMessageAllay = [...allMessgeArray];
+    currentMessageAllay.splice(index, 1);
+    setAllMessgeArray(currentMessageAllay);
+    console.log(allMessgeArray);
+  }
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,15 +26,28 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
+            value={message}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={handleSubmit}>
+          Submit
+        </button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {allMessgeArray.map((data, index) => (
+          <div key={index} className="message">
+            <h1>{data}</h1>
+            <button
+              className="delete-button"
+              onClick={() => handleClose(index)}
+            >
+              x
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
